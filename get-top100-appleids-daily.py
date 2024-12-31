a25ca315f4b55694f6c0ac090ae791deccade24f
HeisenBerg? <admin@tiktokastudio.com>
2025-01-01 07:52:46 +0800

    Update get-top100-appleids-daily.py

--- get-top100-appleids-daily.py
+++ get-top100-appleids-daily.py
@@ -2,6 +2,7 @@
 #https://apps.apple.com/us/charts/iphone/health-fitness-apps/6013?chart=top-paid
 # read category from config
 
+#这样子从wayback拿到的链接并不全 还是得直接读html
 # list all appleid url under this category
 
 # run scrape of each apple id
@@ -48,17 +49,17 @@ async def get_urls_from_archive(domain, start, end):
     domainname = domain.replace("https://", "")
     domainname=domainname.replace('/','-')
     
-    csv_filepath = f'{RESULT_FOLDER}/{domainname}.csv'
+    csv_filepath = f'{RESULT_FOLDER}/top-app-{domainname}.csv'
     csv_file=Recorder(csv_filepath)
 
     fieldnames = ['timestamp', 'url']
     if not os.path.exists(csv_filepath):
         csv_file.add_data(fieldnames)
 
-    query_url = f"http://web.archive.org/cdx/search/cdx?url={subs_wildcard}{domain}/&fl=timestamp,original"
-    filter = f"&statuscode=200&from={start}&to={end}" if end else f"&statuscode=200&from={start}"
-    query_url += "&collapse=urlkey" + filter
-    # query_url=query_url+'&matchType=prefix'
+    query_url = f"http://web.archive.org/cdx/search/cdx?url={domain}/&fl=timestamp,original"
+    # filter = f"&statuscode=200&from={start}&to={end}" if end else f"&statuscode=200&from={start}"
+    query_url += "&collapse=urlkey"
+    query_url=query_url+'&matchType=prefix'
 
     headers = {
         'Referer': 'https://web.archive.org/',
@@ -69,7 +70,7 @@ async def get_urls_from_archive(domain, start, end):
         async with aiohttp.ClientSession(connector=None) as session:
             async with session.get(query_url, headers=headers, 
                                    proxy='http://127.0.0.1:1080',
-                                   timeout=300) as resp:
+                                   timeout=3000) as resp:
                 if resp.status != 200:
                     print(f"Received status code {resp.status}.")
                     return
@@ -103,6 +104,7 @@ async def fetch_urls_for_domain(domain, start, end):
     for attempt in range(retries):
         try:
             await get_urls_from_archive(domain, start, end)
+
             break  # Break if successful
         except (aiohttp.ClientError, asyncio.TimeoutError) as e:
             print(f"Connection error on attempt {attempt + 1}: {e}")
@@ -116,24 +118,37 @@ def extract_urls(domain):
     domainname=domainname.replace('/','-')
 
 
-    csv_file = f'{RESULT_FOLDER}/.csv'
+    csv_file = f'{RESULT_FOLDER}/top-app-{domainname}.csv'
+    if not os.path.exists(csv_file):
+        print(f'{domain} category urls file not prepared yet')
+        return
     df=pd.read_csv(csv_file)
     urls=df['url']
-    urls=[x if x and 'top-' in x  else None for x in urls]
+    if len(urls)==0:
+        return 
+    urls=[x if x and '?chart=top-' in x  else None for x in urls]
     urls=list(set(urls))
-    print(urls)
-    for t in ['paid','free']:
-        urls=[x if x and '-'+t in x else None for x in urls]
+    if len(urls)==0:
+        return 
+    # print(urls)
+    freedata=[x if x and 'chart=top-free' in x else None for x in urls]
+    paiddata=[x if x and 'chart=top-paid' in x else None for x in urls]
+
     # freeurls=[x if x and  '-'+t in x else None for x in urls]
-        out_filepath=f'{RESULT_FOLDER}/ipad-top-{t}.csv'
 
-        if '/iphone/' in domain:
-            out_filepath=f'{RESULT_FOLDER}/iphone-top-{t}.csv'
+    free_filepath=f'{RESULT_FOLDER}/top-app-free-{domainname}.csv'
+    paid_filepath=f'{RESULT_FOLDER}/top-app-paid-{domainname}.csv'
+
   
-        out_file=Recorder(out_filepath)
+    free_out_file=Recorder(free_filepath)
+    paid_out_file=Recorder(paid_filepath)
+    for url in freedata:
 
-        out_file.add_data(urls)
-        out_file.record()
+        free_out_file.add_data(url)
+    for url in paiddata:
+        paid_out_file.add_data(url)
+    free_out_file.record()
+    paid_out_file.record()
 
 async def main():
     """Main entry point to handle asynchronous execution."""
@@ -151,6 +166,10 @@ async def main():
             tasks.append(task)
 
     await asyncio.gather(*tasks)
+    print('post processing urls')
+    for domain in DOMAIN_LIST:
+        extract_urls(domain)
+
     print(f"Completed in {time.time() - start_time} seconds.")
 
 
