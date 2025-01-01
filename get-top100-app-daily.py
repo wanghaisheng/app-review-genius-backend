262d31eb29620a489a989750fe30be12f0157cef
HeisenBerg? <admin@tiktokastudio.com>
2025-01-01 23:10:52 +0800

    Update get-top100-app-daily.py

--- get-top100-app-daily.py
+++ get-top100-app-daily.py
@@ -40,11 +40,8 @@ def insert_into_d1(data):
         "Content-Type": "application/json"
     }
 
-    sql_query = "INSERT INTO ios_app_data (platform, type, cid, cname, rank, appid,appname, icon, link, title, updateAt,country) VALUES "
-    values = ", ".join([
-        f"('{row['platform']}', '{row['type']}', '{row['cid']}', '{row['cname']}', {row['rank']}, '{row['appid']}','{row['appname']}' '{row['icon']}', '{row['link']}', '{row['title']}', '{row['updateAt']}','{row['country']}')"
-        for row in data
-    ])
+    sql_query = "INSERT INTO ios_app_data (platform, type, cid, cname, rank, appid, appname, icon, link, title, updateAt, country) VALUES "
+    values = ", ".join([f"('{row['platform']}', '{row['type']}', '{row['cid']}', '{row['cname']}', {row['rank']}, '{row['appid']}','{row['appname']}', '{row['icon']}', '{row['link']}', '{row['title']}', '{row['updateAt']}','{row['country']}')" for row in data])
     sql_query += values + ";"
 
     payload = {"sql": sql_query}
@@ -123,7 +120,7 @@ def getids_from_category(url, outfile):
     """
     Extract app details from a category URL.
     """
-    print('get id for category url',url)
+    print('get id for category url', url)
     try:
         tab = browser.new_tab()
         cid = url.split('/')[-1]
@@ -132,7 +129,6 @@ def getids_from_category(url, outfile):
         country = url.split('/')[-5]
 
         for chart_type in ['chart=top-free', 'chart=top-paid']:
-            # print('check urls',chart_type)
             type = chart_type.split('-')[-1]
             full_url = f"{url}?{chart_type}"
             tab.get(full_url)
@@ -140,22 +136,19 @@ def getids_from_category(url, outfile):
             links = tab.ele('.l-row chart').children()
             for link in links:
                 app_link = link.ele('tag:a').link
-                # print('there is aapp',app_link)                
                 icon = link.ele('.we-lockup__overlay').ele('t:img').link
                 if app_link is None:
                     return 
-                appname=app_link.split('/')[-2]
-                print('name',appname)
+                appname = app_link.split('/')[-2]
                 rank = link.ele('.we-lockup__rank').text
                 title = link.ele('.we-lockup__text ').text
-                # print('try to add data')
                 outfile.add_data({
                     "platform": platform,
-                    "country":country,
+                    "country": country,
                     "type": type,
                     "cid": cid,
                     "cname": cname,
-                    "appname":appname,
+                    "appname": appname,
                     "rank": rank,
                     "appid": app_link.split('/')[-1],
                     "icon": icon,
@@ -163,48 +156,23 @@ def getids_from_category(url, outfile):
                     "title": title,
                     "updateAt": datetime.now()
                 })
-                print('add app',app_link)
+                print('add app', app_link)
 
     except Exception as e:
         print(f"Error processing category URL {url}: {e}")
 
 
-async def get_urls_from_archive(domain, start, end):
+async def get_review(item, outfile):
     """
-    Fetch URLs from the Wayback Machine.
+    Asynchronously fetch the review for the given app and save it to the outfile.
     """
-    try:
-        domainname = domain.replace("https://", "").replace('/', '-')
-        csv_filepath = f'{RESULT_FOLDER}/top-app-{domainname}.csv'
-        csv_file = Recorder(csv_filepath)
+    app = AppStore(country=item['country'], app_name=item['appname'])
+    await asyncio.to_thread(app.review, sleep=random.randint(3, 6))  # Run in a separate thread to avoid blocking
 
-        query_url = f"http://web.archive.org/cdx/search/cdx?url={domain}/&fl=timestamp,original&collapse=urlkey&matchType=prefix"
-        headers = {
-            'Referer': 'https://web.archive.org/',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-        }
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(query_url, headers=headers, timeout=3000) as resp:
-                resp.raise_for_status()
-                buffer = bytearray()
-                while True:
-                    chunk = await resp.content.read(1024)
-                    if not chunk:
-                        if buffer:
-                            process_line(csv_file, [buffer.decode('utf-8', 'replace')])
-                        break
-                    buffer.extend(chunk)
-    except Exception as e:
-        print(f"Error fetching archive URLs for {domain}: {e}")
-
-def getReivew(item,outfile):
-    app = AppStore(country=item['country'],app_name=item['appname'])
-    app.review(sleep = random.randint(3,6))
     for review in app.reviews:
-        item['score']= review['rating']
-        item['userName']= review['userName']
-        item['review']= review['review'].replace('\r',' ').replace('\n',' ')
+        item['score'] = review['rating']
+        item['userName'] = review['userName']
+        item['review'] = review['review'].replace('\r', ' ').replace('\n', ' ')
         outfile.add_data(item)
 
 
@@ -212,44 +180,44 @@ async def main():
     """
     Main entry point for asynchronous execution.
     """
-    saved1=False
-    downloadreview=True
+    saved1 = False
+    downloadreview = True
     try:
         os.makedirs(RESULT_FOLDER, exist_ok=True)
         current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
         outfile_path = f'{RESULT_FOLDER}/top-100-app-{current_time}.csv'
         outfile = Recorder(outfile_path)
+        
         for domain in DOMAIN_LIST:
             print(f"Processing domain: {domain}")
             category_urls = get_category_urls(domain)
-            print(f'founding category urls:{len(category_urls)}')
-            category_urls=[
-
-                'https://apps.apple.com/us/charts/iphone/health-fitness-apps/6013',
-            ]
+            print(f'found category urls: {len(category_urls)}')
             for url in category_urls:
                 getids_from_category(url, outfile)
         outfile.record()
-        print('get id ok',outfile_path)
+        print('get id ok', outfile_path)
 
-        if saved1==True:
+        if saved1:
             save_csv_to_d1(outfile_path)
-# get reviews
-        outfile_reviews_path = f'{RESULT_FOLDER}/top-100-app-reviews-{current_time}.csv'
-        outfile_reviews = Recorder(outfile_reviews_path)
-        if downloadreview==False:
-            return 
-
-        df=pd.read_csv(outfile_path)
-        lang='en'
-        for index, row in df.iterrows():
         
-            getReivew(row,outfile)
+        # Get reviews concurrently
+        if downloadreview:
+            outfile_reviews_path = f'{RESULT_FOLDER}/top-100-app-reviews-{current_time}.csv'
+            outfile_reviews = Recorder(outfile_reviews_path)
 
-        outfile_reviews.record()
+            df = pd.read_csv(outfile_path)
+            tasks = []  # List of tasks for concurrent execution
 
-    
+            # Create tasks for each row in the DataFrame
+            for index, row in df.iterrows():
+                tasks.append(get_review(row, outfile_reviews))
+
+            # Run all review tasks concurrently
+            await asyncio.gather(*tasks)
+
+            outfile_reviews.record()
+
     except Exception as e:
         print(f"Error in main execution: {e}")
 
