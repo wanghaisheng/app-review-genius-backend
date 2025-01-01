e56e5c0b901479377b56f71c4e0473ca0bd1da2f
HeisenBerg? <admin@tiktokastudio.com>
2025-01-02 04:30:20 +0800

    Update get-top100-app-daily.py

--- get-top100-app-daily.py
+++ get-top100-app-daily.py
@@ -8,7 +8,7 @@ from aiohttp_socks import ProxyType, ProxyConnector, ChainProxyConnector
 from DataRecorder import Recorder
 import pandas as pd
 from getbrowser import setup_chrome
-from SearchAppId import SearchAppId
+from searchappid import SearchAppId
 from app_store_scraper import AppStore
 import requests
 import random
