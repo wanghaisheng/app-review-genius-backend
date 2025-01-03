5c66a34d5827100e452cb996ea4ad43b6f0de9ed
HeisenBerg? <admin@tiktokastudio.com>
2025-01-03 18:31:31 +0800

    Update saveReviewtoD1.py

--- saveReviewtoD1.py
+++ saveReviewtoD1.py
@@ -1,5 +1,12 @@
 import hashlib
 import requests
+D1_DATABASE_ID = os.getenv('D1_APP_DATABASE_ID')
+CLOUDFLARE_ACCOUNT_ID = os.getenv('CLOUDFLARE_ACCOUNT_ID')
+CLOUDFLARE_API_TOKEN = os.getenv('CLOUDFLARE_API_TOKEN')
+
+
+CLOUDFLARE_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{CLOUDFLARE_ACCOUNT_ID}/d1/database/{D1_DATABASE_ID}"
+
 
 def compute_hashes(data):
     """
