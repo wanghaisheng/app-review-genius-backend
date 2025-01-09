217d3fe6b387202c4ac19b444d78aee02e435b16
HeisenBerg? <admin@tiktokastudio.com>
2025-01-10 05:29:07 +0800

    Update keywordsearchappreviews.py

--- keywordsearchappreviews.py
+++ keywordsearchappreviews.py
@@ -185,7 +185,7 @@ async def get_review(url, outfile, keyword):
         
         app = AppStore(country=country, app_name=appname)
 
-        await asyncio.to_thread(app.review, sleep=random.randint(0, 1))
+        await asyncio.to_thread(app.review(limit=1000), sleep=random.randint(0, 1))
         all_reviews=app.reviews
         print('manual get review')
         items=[]
