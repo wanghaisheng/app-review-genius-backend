d8886f309cd5dc38fb7321b6b93af11ae1940f41
HeisenBerg? <admin@tiktokastudio.com>
2025-05-10 06:55:09 +0800

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -112,7 +112,8 @@ def app_reviews():
                     apple_app_package_url=apple_app_package_url.split('?')[0]
                 
                 apple_app_package_name=apple_app_package_url.split('/')[-2]
-                if  len(apple_app_package_name)==0:
+                if not len(apple_app_package_name)>0:
+
                     print('not support package,',apple_app_package_url,apple_app_package_name) 
                     return 
                 app_store_scraper(apple_app_package_name,country,lang)
